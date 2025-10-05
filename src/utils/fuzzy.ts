594ad0b873a334e29b329f758fe8b9b1a9932624
marunanya <marunatyan@163.com>
2025-10-05 10:50:42 +0800

    update

--- src/utils/fuzzy.ts
+++ src/utils/fuzzy.ts
@@ -1,22 +1,30 @@
 export function fuzzySearch(strings: string[], pattern: string) {
     const lowerPattern = pattern.toLowerCase()
+    const mostUpperCount = Math.max(...strings.map(str => Array.from(str).filter(char => isUpper(char)).length))
+    const mostStringLength = Math.max(...strings.map(str => str.length))
     return strings.map(str => {
         const lowerStr = str.toLowerCase()
-        const upperMap = Array.from(str).map(char => char === char.toUpperCase())
-        const [score, matchedPositions] = fuzzyScore(lowerStr, lowerPattern, upperMap, 0, 0, [])
+        const upperPositions = Array.from(str).map((char, index) => isUpper(char) ? index : -1).filter(i => i >= 0)
+        const [score, matchedPositions] = fuzzyScore(lowerStr, lowerPattern, mostStringLength, upperPositions, mostUpperCount, 0, 0, [])
         return [str, score, matchedPositions] as [string, number, number[]]
     }).filter(([_, score, __]) => score > 0).sort((a, b) => b[1] - a[1])
 }
 
-function fuzzyScore(lowerStr: string, lowerPattern: string, upperMap: boolean[], offset: number, baseScore: number, matchedPositions: number[]): [number, number[]] {
+function isUpper(char: string) {
+    return char === char.toUpperCase() && char !== char.toLowerCase()
+}
+
+function fuzzyScore(lowerStr: string, lowerPattern: string, mostStringLength: number, upperPositions: number[], mostUpperCount: number, offset: number, baseScore: number, matchedPositions: number[]): [number, number[]] {
     let bestScore = 0
     let bestMatchedPositions: number[] = []
     for (let i = 0; i < lowerStr.length; ++i) {
         if (lowerStr[i] === lowerPattern[0]) {
-            const score = upperMap[offset + i] ? 1 : (lowerStr.length - i) / lowerStr.length
+            upperPositions = upperPositions.filter(index => index >= offset)
+            const upperIndex = upperPositions.indexOf(offset + i)
+            const score = Math.pow(upperIndex < 0 ? 1 - i / mostStringLength : 1 - upperIndex / mostUpperCount / mostStringLength, 2)
             const [tempScore, tempPositions] = lowerPattern.length == 1 ?
                 [baseScore + score, [...matchedPositions, offset + i]] :
-                fuzzyScore(lowerStr.substring(i + 1), lowerPattern.substring(1), upperMap, offset + i + 1, baseScore + score, [...matchedPositions, offset + i])
+                fuzzyScore(lowerStr.substring(i + 1), lowerPattern.substring(1), mostStringLength, upperPositions, mostUpperCount, offset + i + 1, baseScore + score, [...matchedPositions, offset + i])
             if (tempScore > bestScore) {
                 bestScore = tempScore
                 bestMatchedPositions = tempPositions
